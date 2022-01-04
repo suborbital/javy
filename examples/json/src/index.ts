d676bf86af0dae7f622b7f2db7cba11a8ccb1141
Oscar Spencer <oscar@grain-lang.org>
2022-01-04 15:32:32 -0500

    Add json example

---
+++ examples/json/src/index.ts
@@ -0,0 +1,14 @@
+import "fast-text-encoding";
+import { run } from "./lib";
+
+declare var TextEncoder: any;
+declare var TextDecoder: any;
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+export const main = (payload: ArrayBuffer): ArrayBuffer => {
+  let input = JSON.parse(decoder.decode(payload));
+  let output = JSON.stringify(run(input));
+  return encoder.encode(output).buffer;
+};
